8328578c672b1242e78f30356bb8e6f60d9d385d
YungJimbo <167120302+YungJimbo@users.noreply.github.com>
2024-04-26 15:01:48 -0500

    Create Empresas.jsx

---
+++ src/Componentes/Empresas/Empresas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Empresas() {
+  return (
+    <>
+      <MainDiv />
+      <MarcasSlider />
+      <Counts />
+      <Dirigido />
+    </>
+  )
+}
+
+export default Empresas
