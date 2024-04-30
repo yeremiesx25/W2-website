605e7ea3aa5358281a6e1c4db8a4e0a6e82285fb
Alexander0127 <AlexanderTR0127@gmail.com>
2024-04-29 19:50:55 -0500

    página error

--- src/Componentes/Principal/Hero.jsx
+++ src/Componentes/Principal/Hero.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion"
 
 function Hero() {
   return (
-<<<<<<< HEAD
         <div className='bg-secundarycolor rounded-xl flex justify-center items-center flex-wrap-reverse md:w-3/4 p-4 mr-2 ml-2 self-center' style={{ maxWidth: '1000px', minHeight: '437px' }}>
             <div className='w-full md:w-3/4 lg:w-1/2 h-full flex items-center justify-center flex-col'>
                 {/* Texto con ajustes responsivos */}
@@ -26,11 +25,9 @@ function Hero() {
                 {/* Imagen con ajustes responsivos */}
                 <img src={vector} alt="" className='w-48 md:w-76 h-auto' />
             </motion.div>
-=======
         <div className='w-[90%] h-[40rem] md:w-[70rem] bg-primarycolor rounded-xl mt-12 flex justify-center items-center flex-wrap p-2'>
           <h2 className='text-white font-medium text-xl md:text-2xl text-center'>Vive la experiencia del reclutamiento</h2>
           <img src={imagehero} alt="" />
->>>>>>> 12c07a32530f1f9f3c9e7ed65901932de8f532d5
         </div>
   );
 }
