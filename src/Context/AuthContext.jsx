16b0fd3d04ee014da8bbc1e7f3dd4197a5b12c35
Alexander0127 <AlexanderTR0127@gmail.com>
2024-05-16 10:42:41 -0500

    asas

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,16 @@
+import {createContext, useState} from 'react'}
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) =>{
+    const [user, setUser]  = useState([]);
+    async function signInWithGoogle() {
+        try {
+        const { data, error } = supabase.auth.signInWithOAuth({
+            provider: 'google',
+          });
+          if(error) throw new Error("A ocurrido un error durante la autenticación");
+          return data;
+        } catch (error) {}  
+    }
+};
