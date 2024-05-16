8561f11e2872c661c3334fdd604973875870ab9a
Alexander0127 <AlexanderTR0127@gmail.com>
2024-05-16 12:10:43 -0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 //importar componentes
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Componentes/Principal/Home.jsx'
-import Empresas from './Componentes/Empresas/Empresas.jsx'
-import Power from './Componentes/Power/Power.jsx'
-import Practicantes from './Componentes/Practicantes/Practicantes.jsx'
-import ScrollToTop from './Componentes/Principal/ScrollToTop.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Componentes/Principal/Home.jsx";
+import Empresas from "./Componentes/Empresas/Empresas.jsx";
+import Power from "./Componentes/Power/Power.jsx";
+import Practicantes from "./Componentes/Practicantes/Practicantes.jsx";
+import ScrollToTop from "./Componentes/Principal/ScrollToTop.jsx";
+import { AuthContextProvider } from "./Context/AuthContext.jsx";
 
 function App() {
   return (
-<BrowserRouter >
-<ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Empresas" element={<Empresas />} />
-        <Route path="/Power" element={<Power />} />
-        <Route path="/Practicantes" element={<Practicantes />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthContextProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Empresas" element={<Empresas />} />
+          <Route path="/Power" element={<Power />} />
+          <Route path="/Practicantes" element={<Practicantes />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
--- src/Componentes/Power/Login.jsx
+++ src/Componentes/Power/Login.jsx
@@ -1,73 +1,16 @@
 import React from 'react'
+import { UserAuth } from '../../Context/AuthContext'
 
 function Login() {
+    const {signInWithGoogle} = UserAuth();
   return (
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
-    <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/301692/login.svg" alt="Workflow"/>
-        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-            Sign in to your account
-        </h2>
-        <p class="mt-2 text-center text-sm leading-5 text-blue-500 max-w">
-            Or
-            <a href="#"
-                class="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                create a new acccount
-            </a>
-        </p>
-    </div>
-
-
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form>
-                <div>
-                    <label for="email" class="block text-sm font-medium leading-5  text-gray-700">Email address</label>
-                    <div class="mt-1 relative rounded-md shadow-sm">
-                        <input id="email" name="email" placeholder="user@example.com" type="email" required="" value="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                        <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd"
-                                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mt-6">
-                    <label for="password" class="block text-sm font-medium leading-5 text-gray-700">Password</label>
-                    <div class="mt-1 rounded-md shadow-sm">
-                        <input id="password" name="password" type="password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"/>
-                    </div>
-                </div>
-
-                <div class="mt-6 flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember_me" name="remember" type="checkbox" value="1" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"/>
-                        <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">Remember me</label>
-                    </div>
-
-                    <div class="text-sm leading-5">
-                        <a href="#"
-                            class="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                            Forgot your password?
-                        </a>
-                    </div>
-                </div>
-
-                <div class="mt-6">
-                    <span class="block w-full rounded-md shadow-sm">
-            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-              Sign in
-            </button>
-          </span>
-                </div>
-            </form>
-
-        </div>
-    </div>
-</div>
+    <button onClick={signInWithGoogle}
+     type="button" class="py-2 px-4 max-w-md flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+  <svg width="20" height="20" fill="currentColor" class="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+    <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
+  </svg>
+  Sign in with Google
+</button>
   )
 }
 
