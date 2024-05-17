4621ee72bb8d1e51c276250002fea3e22431eab1
Alexander0127 <AlexanderTR0127@gmail.com>
2024-05-17 15:29:18 -0500

    asd

--- src/Componentes/Practicantes/CtaAreas.jsx
+++ src/Componentes/Practicantes/CtaAreas.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function CtaAreas() {
   return (
-    
-<div class="bg-white dark:bg-gray-800 font-dmsans">
-    <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:pt-16 lg:px-8">
+    <div class="bg-white dark:bg-gray-800 font-dmsans">
+      <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:pt-16 lg:px-8">
         <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
-            <span class="block">
-                Conoce nuestras áreas,
-            </span>
-            <span class="block text-primarycolor">
-                Sé parte de nuestro equipo
-            </span>
+          Descubre nuestras áreas,
+          <span class=" text-primarycolor"> sé parte de nuestro equipo</span>
         </h2>
-        <p class="text-xl mt-4 max-w-md mx-auto text-gray-600">
-        Emocionantes proyectos, mentoría de expertos y un ambiente inclusivo que fomenta el aprendizaje y el crecimiento profesional
+        <p class="text-xl mt-4  mx-auto text-gray-600">
+          Emocionantes proyectos, mentoría de expertos y un ambiente inclusivo
+          que fomenta el aprendizaje y el crecimiento profesional
         </p>
+      </div>
     </div>
-</div>
-
-  )
+  );
 }
 
-export default CtaAreas
+export default CtaAreas;
