2db73010864ef80faa54c4df064dd88efd5a37d6
PieroNF <pgianmarco10@gmail.com>
2024-05-17 15:25:40 -0500

    modal

--- src/Componentes/Power/HeaderPower.jsx
+++ src/Componentes/Power/HeaderPower.jsx
@@ -6,12 +6,11 @@ import logo from '../../assets/Logo Power.png';
 
 function HeaderPower() {
   const [showMenu, setShowMenu] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
-
-  const [modalOpen, setModalOpen] = useState(false);
 
   const handleModalOpen = () => {
     setModalOpen(true);
@@ -23,13 +22,13 @@ function HeaderPower() {
 
   return (
     <header className="bg-gray-900 text-gray-50 font-dmsans flex justify-around w-full flex-wrap fixed z-10" style={{ height: '78px' }}>
-    <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-      {/* Logo */}
-      <div className="flex items-center">
-        <a href="/Power">
-          <img src={logo} alt="Power" className="w-16 h-auto" />
-        </a>
-      </div>
+      <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+        {/* Logo */}
+        <div className="flex items-center">
+          <a href="/Power">
+            <img src={logo} alt="Power" className="w-16 h-auto" />
+          </a>
+        </div>
 
         {/* Icono de menú para dispositivos móviles */}
         <div className="md:hidden w-full flex justify-end flex-wrap">
@@ -47,27 +46,27 @@ function HeaderPower() {
           <Link to="/Power" className="font-semibold text-amber-400 hover:text-white underline decoration-amber-400 underline-offset-4">Power</Link>
         </div>
 
-        {/* Botones de login y registro */}
-        <div className="flex items-center">
-          <button onClick={handleModalOpen} className="text-sm text-amber-400 hover:text-white hidden md:flex">Iniciar sesión</button>
-          <button className="text-sm bg-amber-400 hover:bg-amber-600 text-white px-4 py-2 rounded-lg ml-4 hidden md:flex">Registrarse</button>
+        {/* Botones de login y registro (solo en escritorio) */}
+        <div className="hidden md:flex items-center">
+          <button onClick={handleModalOpen} className="text-sm text-amber-400 hover:text-white">Iniciar sesión</button>
+          <button className="text-sm bg-amber-400 hover:bg-amber-600 text-white px-4 py-2 rounded-lg ml-4">Registrarse</button>
         </div>
       </div>
 
       {/* Menú desplegable para dispositivos móviles */}
       {showMenu && (
-        <div className="md:hidden bg-primarytext w-full">
+        <div className="md:hidden bg-gray-900 w-full">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link to="/" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Inicio</Link>
             <Link to="/Empresas" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Empresas</Link>
             <Link to="/Power" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Power</Link>
             <Link to="/Practicantes" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Descubriendo Talentos</Link>
-            <button className="bg-amber-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-2 mr-2">Iniciar</button>
+            <button onClick={handleModalOpen} className="bg-amber-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-2 mr-2">Iniciar Sesión</button>
             <button className="bg-amber-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg mt-2">Registrar</button>
           </div>
         </div>
       )}
-      {modalOpen && (<Login closeModal={handleModalClose} />)}
+      {modalOpen && <Login closeModal={handleModalClose} />}
     </header>
   );
 }
