859dd3027673e63d56eafe43b4924b68bcdd177f
PieroNF <pgianmarco10@gmail.com>
2024-05-30 14:24:00 -0500

    z

--- src/Componentes/Principal/Privacidad.jsx
+++ src/Componentes/Principal/Privacidad.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from '../Principal/Navbar'
-import Footer from '../Principal/Footer'
-import BtnWsp from '../Principal/BtnWsp'
+import Navbar from '.Navbar'
+import Footer from '.Footer'
+import BtnWsp from './BtnWsp'
 
 function Home() {
   return (
