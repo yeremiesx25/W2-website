248b07b956e097a6af927939eb8b1cb36fec9c40
PieroNF <pgianmarco10@gmail.com>
2024-05-30 14:06:51 -0500

    modal

--- src/Componentes/Power/CardTrabajo.jsx
+++ src/Componentes/Power/CardTrabajo.jsx
@@ -1,16 +1,6 @@
 import React, { useState } from 'react';
-import startup from '../../assets/STARTUP TALENT.png';
-import { FaMapMarkerAlt } from 'react-icons/fa';
-import flyer2 from '../../assets/Flyers Nuevos/2.png'
-import flyer3 from '../../assets/Flyers Nuevos/3.png'
-import flyer4 from '../../assets/Flyers Nuevos/4.png'
-import flyer5 from '../../assets/Flyers Nuevos/5.png'
-import flyer6 from '../../assets/Flyers Nuevos/6.png'
-import flyer7 from '../../assets/Flyers Nuevos/7.png'
-import flyer8 from '../../assets/Flyers Nuevos/8.png'
-import flyer9 from '../../assets/Flyers Nuevos/9.png'
 
-function CardTrabajo({ jobTitle, company, location, salary, companyLogo, requirements, timeActive, onClick}) {
+function CardTrabajo({ jobTitle, company, location, salary, companyLogo, requirements, timeActive, onClick, imageUrl }) {
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleModalOpen = () => {
@@ -23,35 +13,32 @@ function CardTrabajo({ jobTitle, company, location, salary, companyLogo, require
 
   return (
     <div className='w-full flex justify-center font-dmsans'>
-      <button onClick={handleModalOpen}  className="w-full md:w-[90%] bg-white text-left border hover:shadow-md rounded-lg overflow-hidden">
-            <div className="px-6 py-4">
-                <div className="flex items-center mb-2">
-                    <img className="w-10 h-10 rounded-full mr-4" src={companyLogo} alt={company} />
-                    <div>
-                        <div className="font-bold text-xl">{jobTitle}</div>
-                        <p className="text-gray-700">{company}</p>
-                    </div>
-                </div>
-                <p className="text-gray-700">{location}</p>
-                <p className="text-gray-700 mt-2"><span className="font-bold">Sueldo:</span> {salary}</p>
+      <button onClick={handleModalOpen} className="w-full md:w-[90%] bg-white text-left border hover:shadow-md rounded-lg overflow-hidden">
+        <div className="px-6 py-4">
+          <div className="flex items-center mb-2">
+            <img className="w-14 h-14 rounded-full mr-4" src={companyLogo} alt={company} />
+            <div>
+              <div className="font-bold text-xl">{jobTitle}</div>
+              <p className="text-gray-700">{company}</p>
             </div>
-            <div className="px-6 py-4 bg-amber-400">
-                <span className="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{timeActive} activo</span>
-            </div>
-        </button>
+          </div>
+          <p className="text-gray-700">{location}</p>
+          <p className="text-gray-700 mt-2"><span className="font-bold">Sueldo:</span> {salary}</p>
+        </div>
+        <div className="px-6 py-4 bg-amber-400">
+          <span className="inline-block bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{timeActive} activo</span>
+        </div>
+      </button>
       
       {modalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="bg-white p-8 rounded-lg z-50">
-            <h2 className="text-2xl font-bold mb-4">Detalles del Trabajo</h2>
-            <p>Descripción del trabajo...</p>
-            <img className='w-96' src="https://scontent.fpio3-1.fna.fbcdn.net/v/t39.30808-6/430204572_937010811761844_8302191555907491683_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEVAeVBIFRsqcYmqMPaRMw46roinPyg0k3quiKc_KDSTb_nOt3Pw3sxilVdoaFH71vsNexe7SSyj_bCdSgOzr_2&_nc_ohc=bl5ZxZAY3nkQ7kNvgG2guid&_nc_ht=scontent.fpio3-1.fna&oh=00_AYAz-FTQhrckEIzmS6-EPC8DAEm7bFkMJevxhJzyDk3-IA&oe=6644604C" alt="" />
-            <div className='flex w-full justify-around'>
-            <button onClick={handleModalClose} className="mt-4 bg-red-600 text-white px-4 py-2 rounded-lg font-semibold">Cerrar</button>
-            <button onClick={handleModalClose} className="mt-4 bg-primarytext text-white px-4 py-2 rounded-lg font-semibold">Postular</button>
+          <div className="bg-white p-4 md:p-8 rounded-lg z-50 w-11/12 md:w-2/3 lg:w-1/2 max-w-xl overflow-y-auto">
+            <img className='w-full h-auto mb-4 rounded' src={imageUrl} alt="Job Details" />
+            <div className='flex w-full justify-around mt-4'>
+              <button onClick={handleModalClose} className="bg-red-600 text-white px-4 py-2 rounded-lg font-semibold">Cerrar</button>
+              <button onClick={handleModalClose} className="bg-primarytext text-white px-4 py-2 rounded-lg font-semibold">Postular</button>
             </div>
-            
           </div>
         </div>
       )}
--- src/Componentes/Power/TrabajosContainer.jsx
+++ src/Componentes/Power/TrabajosContainer.jsx
@@ -4,8 +4,17 @@ import React, { useState } from 'react';
 import Buscador from './Buscador';
 import CardTrabajo from './CardTrabajo';
 import InfoJob from '../PowerAuth/InfoJob'; // Importa el nuevo componente
-import startup from '../../assets/STARTUP TALENT.png';
+import agil from "../../assets/AGIL TALENT.png";
 import flecha from "../../assets/flecha.png";
+
+import asesor from "../../assets/Flyers Nuevos/2.png";
+import repartidor from "../../assets/Flyers Nuevos/3.png";
+import conductor from "../../assets/Flyers Nuevos/4.png";
+import medico from "../../assets/Flyers Nuevos/5.png";
+import operario from "../../assets/Flyers Nuevos/6.png";
+import asistente from "../../assets/Flyers Nuevos/7.png";
+import almacenero from "../../assets/Flyers Nuevos/8.png";
+import trailer from "../../assets/Flyers Nuevos/9.png";
 
 function TrabajosContainer() {
   const jobRequirements = ["Experiencia en React.js", "Experiencia en desarrollo asdyyyy", "Habilidades de comunicación"];  
@@ -18,18 +27,21 @@ function TrabajosContainer() {
 
   return (
     <div id='ofertas' className='w-full flex flex-col items-center font-dmsans pt-12 px-4 justify-center'>
-          <h2 class="font-dmsans sm:text-4xl text-2xl font-bold title-font text-center text-transparent bg-clip-text bg-gradient-to-r from-primarytext to-black mb-12 mt-6"> <img src={flecha} alt="" className="inline-block w-16 h-12 mr-2"></img> Ofertas <span class="text-amber-500 xl:inline"> Laborales </span></h2>
+      <h2 className="font-dmsans sm:text-4xl text-2xl font-bold title-font text-center text-transparent bg-clip-text bg-gradient-to-r from-primarytext to-black mb-12 mt-6"> 
+        <img src={flecha} alt="" className="inline-block w-16 h-12 mr-2" />
+        Ofertas <span className="text-amber-500 xl:inline"> Laborales </span>
+      </h2>
       <div className='flex w-full justify-center items-center'>
-        <div className='flex flex-col w-full md:w-1/2  justify-start items-center py-4 gap-4 h-[650px] overflow-auto'>
+        <div className='flex flex-col w-full md:w-1/2 justify-start items-center py-4 gap-4 h-[650px] overflow-auto'>
           <CardTrabajo
-            
-            jobTitle="QF - Analista de Aseguramiento de la Calidad "
-            company="Dicar"
-            location="Chorrillos"
-            salary="S/. 3500"
-            companyLogo={startup}
+            jobTitle="Asesor de Ventas"
+            company=""
+            location="Lima Este - Sur - Centro"
+            salary="S/. 1025"
+            companyLogo={agil}
             requirements={['Químico farmacéutico titulado', 'Residir en Chorrillos o zonas aledañas', 'Experiencia de 1 año como Analista de Aseguramiento de la Calidad.']}
             timeActive="Hace 2 días"
+            imageUrl={asesor}
             onClick={() => handleJobClick({
               jobTitle: "QF - Analista de Aseguramiento de la Calidad",
               company: "Mi Empresa",
@@ -41,14 +53,14 @@ function TrabajosContainer() {
             })}
           />
           <CardTrabajo
-            
-            jobTitle="Asistente Operativo de Planta Frío"
-            company="Ransa"
-            location="Callao"
-            salary="S/. 1300"
-            companyLogo={startup}
-            requirements={['Con estudios de las carreras Técnicas de Refrigeración o electricidad u carreras afines.', 'Experiencia como asistente operativo en equipos de refigeración.', 'Disponibilidad para realizar turnos rotativos y vivir en el Callao.' ]}
+            jobTitle="Auxiliar de Reparto CBC"
+            company=""
+            location="Huachipa/Ate"
+            salary="S/. 1025 - S/. 1451.50"
+            companyLogo={agil}
+            requirements={['Con estudios de las carreras Técnicas de Refrigeración o electricidad u carreras afines.', 'Experiencia como asistente operativo en equipos de refigeración.', 'Disponibilidad para realizar turnos rotativos y vivir en el Callao.']}
             timeActive="Hace 2 días"
+            imageUrl={repartidor}
             onClick={() => handleJobClick({
               jobTitle: "Asistente Operativo de Planta Frío",
               company: "Ransa",
@@ -60,14 +72,14 @@ function TrabajosContainer() {
             })}
           />
           <CardTrabajo
-            
-            jobTitle="Conductores Semitrailer Americano A3C Y A4"
-            company="Ransa"
-            location="Callao"
-            salary="S/. 1900"
-            companyLogo={startup}
+            jobTitle="Conductor de Reparto A3C - A3B"
+            company=""
+            location="Huachipa - Ate - Comas"
+            salary="S/. 2100"
+            companyLogo={agil}
             requirements={['DNI y licencia A3C vigente +  A4 + Buen récord del conductor.', 'Disponibilidad para rotar.', 'Experiencia manejando Semitrailer Americano y en ruta minera.']}
             timeActive="Hace 2 días"
+            imageUrl={conductor}
             onClick={() => handleJobClick({
               jobTitle: "Conductores Semitrailer Americano A3C Y A4",
               company: "Ransa",
@@ -79,14 +91,14 @@ function TrabajosContainer() {
             })}
           />
           <CardTrabajo
-            
-            jobTitle="Almacenero"
-            company="Ransa"
-            location="Callao"
-            salary="S/. 1025"
-            companyLogo={startup}
+            jobTitle="Representante Médico Junior"
+            company=""
+            location="Zona Sur"
+            salary="S/. 1300"
+            companyLogo={agil}
             requirements={['Experiencia mínima de 6 meses como almacenero.', 'DNI vigente + Vivir en zonas aledañas al Callao.', 'Disponibilidad para realizar turnos rotativos.']}
             timeActive="Hace 2 días"
+            imageUrl={medico}
             onClick={() => handleJobClick({
               jobTitle: "Almacenero",
               company: "Ransa",
@@ -98,14 +110,14 @@ function TrabajosContainer() {
             })}
           />
           <CardTrabajo
-            
-            jobTitle="Auxiliar de reparto"
-            company="Ate - Huachipa"
+            jobTitle="Operario de Almacén"
+            company="1200"
             location="Callao"
             salary=""
-            companyLogo={startup}
+            companyLogo={agil}
             requirements={[]}
             timeActive="Hace 2 días"
+            imageUrl={operario}
             onClick={() => handleJobClick({
               jobTitle: "Desarrollador bcakend Senior",
               company: "Mi Empresa",
@@ -117,14 +129,14 @@ function TrabajosContainer() {
             })}
           />
           <CardTrabajo
-            
-            jobTitle="Asesor de ventas"
-            company="Topsa"
-            location="Lima este - Lima sur - Lima centro"
-            salary=""
-            companyLogo={startup}
+            jobTitle="Asistente Operativo de Planta Frío"
+            company=""
+            location="Callao"
+            salary="S/. 1300"
+            companyLogo={agil}
             requirements={[]}
             timeActive="Hace 2 días"
+            imageUrl={asistente}
             onClick={() => handleJobClick({
               jobTitle: "Desarrollador bcakend Senior",
               company: "Mi Empresa",
@@ -135,9 +147,47 @@ function TrabajosContainer() {
               timeActive: "Hace 2 días"
             })}
           />
-       
-      </div>
-      {/* {selectedJob && <InfoJob selectedJob={selectedJob} />}  */}
+
+<CardTrabajo
+            jobTitle="Almacenero"
+            company=""
+            location="Callao"
+            salary="S/. 1025"
+            companyLogo={agil}
+            requirements={[]}
+            timeActive="Hace 2 días"
+            imageUrl={almacenero}
+            onClick={() => handleJobClick({
+              jobTitle: "Desarrollador bcakend Senior",
+              company: "Mi Empresa",
+              location: "Ciudad, País",
+              salary: "$50,000 - $70,000 al año",
+              companyLogo: startup,
+              requirements: jobRequirements,
+              timeActive: "Hace 2 días"
+            })}
+          />
+
+<CardTrabajo
+            jobTitle="Conductores SemiTrailer Americano A3C y A4"
+            company=""
+            location="Callao"
+            salary="S/. 2900"
+            companyLogo={agil}
+            requirements={[]}
+            timeActive="Hace 2 días"
+            imageUrl={trailer}
+            onClick={() => handleJobClick({
+              jobTitle: "Desarrollador backend Senior",
+              company: "Mi Empresa",
+              location: "Ciudad, País",
+              salary: "$50,000 - $70,000 al año",
+              companyLogo: startup,
+              requirements: jobRequirements,
+              timeActive: "Hace 2 días"
+            })}
+          />
+        </div>
       </div>
     </div>
   );
