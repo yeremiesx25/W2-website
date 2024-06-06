af7590b83c15b870186d9e8ce07b598227c7c1db
PieroNF <pgianmarco10@gmail.com>
2024-06-06 10:53:01 -0500

    cambios

--- src/Componentes/Power/CardTrabajo2.jsx
+++ src/Componentes/Power/CardTrabajo2.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaLocationDot } from "react-icons/fa6";
+import agil from "../../assets/AGIL TALENT.png"; // Importa la imagen
 
 function CardTrabajo2({ job, onSelectJob }) {
     const { puesto, ubicacion, sueldo, fecha_publicacion, imagen_url } = job;
@@ -23,18 +24,21 @@ function CardTrabajo2({ job, onSelectJob }) {
         <div className='w-full flex justify-center font-dmsans'>
             <button onClick={() => onSelectJob(job)} className="w-full md:w-[90%] bg-white text-left border hover:shadow-md rounded-lg overflow-hidden">
                 <div className="h-44 w-full object-cover object-end" style={{ backgroundImage: `url(${imagen_url})`, backgroundSize: 'cover' }}></div>
-                <div className="p-6">
-                    <div className="flex items-baseline">
-                        <div className="mt-2 flex items-center flex-grow">
+                <div className="p-3 flex justify-between">
+                    <div>
+                        <div className="flex items-baseline">
                             <FaLocationDot style={{ color: '#FF7A01' }} />
                             <div className="ml-2 text-orange-600 text-xs uppercase font-semibold tracking-wide" style={{ color: '#FF7A01' }}>
                                 {ubicacion}
                             </div>
                         </div>
-                        <span className="inline-block bg-brown-600 text-white py-1 px-3 text-xs rounded-full uppercase font-semibold tracking-wide self-start md:self-center"style={{ backgroundColor: '#964A00' }}>S/. {sueldo}</span>
+                        <h4 className="mt-4 font-bold text-lg leading-tight truncate" style={{ marginBottom: '10px' }}>{puesto}</h4>
+                        <div className="text-gray-600 text-sm" style={{ color: '#964A00', fontWeight: '600' }}>{calcularTiempoTranscurrido(fecha_publicacion)}</div>
                     </div>
-                    <h4 className="mt-4 font-bold text-lg leading-tight truncate" style={{ marginBottom: '10px' }}>{puesto}</h4>
-                    <div className="text-gray-600 text-sm" style={{ color: '#964A00', fontWeight: '600' }}>{calcularTiempoTranscurrido(fecha_publicacion)}</div>
+                    <div className="flex flex-col items-end">
+                        <span className="inline-block bg-brown-600 text-white py-1 px-3 text-xs rounded-full uppercase font-semibold tracking-wide" style={{ backgroundColor: '#964A00' }}>S/. {sueldo}</span>
+                        <img src={agil} alt="Agil Talent" className="mt-2" style={{ maxWidth: '70px', maxHeight: 'auto' }} /> {/* Agrega la imagen */}
+                    </div>
                 </div>
             </button>
         </div>
--- src/Componentes/PowerAuth/InfoJob.jsx
+++ src/Componentes/PowerAuth/InfoJob.jsx
@@ -4,13 +4,13 @@ import { FaLocationDot } from "react-icons/fa6";
 function InfoJob({ selectedJob }) {
   return (
     <div className="selected-job-info w-1/2 h-[650px] border rounded-lg flex flex-col mx-10">
-      <h2>{selectedJob.puesto}</h2>
       <div className="flex items-center">
-      <span className="inline-block bg-brown-600 text-white py-1 px-3 text-xs rounded-full uppercase font-semibold tracking-wide self-start md:self-center" style={{ backgroundColor: '#964A00', display: 'flex', alignItems: 'center' }}>
-  <FaLocationDot style={{ color: 'white', marginRight: '5px' }} />
-  {selectedJob.ubicacion}
-</span>
-</div>
+        <span className="inline-block bg-brown-600 text-white py-1 px-3 text-xs rounded-full uppercase font-semibold tracking-wide self-start md:self-center" style={{ backgroundColor: '#964A00', display: 'flex', alignItems: 'center' }}>
+          <FaLocationDot style={{ color: 'white', marginRight: '5px' }} />
+          {selectedJob.ubicacion}
+        </span>
+      </div>
+      <h2>{selectedJob.puesto}</h2>
       <p>Empresa: {selectedJob.company}</p>
       <p>Fecha: {selectedJob.fecha_publicacion}</p>
     </div>
