ec46fc1d4602b86cccab933c19795a84073b52d2
PieroNF <pgianmarco10@gmail.com>
2024-07-17 11:32:20 -0500

    as

--- src/Componentes/Power/TrabajosContainer.jsx
+++ src/Componentes/Power/TrabajosContainer.jsx
@@ -3,6 +3,7 @@ import CardTrabajo from './CardTrabajo';
 import InfoJobPower from './InfoJobPower';
 import JobsContext from '../../Context/JobsContext';
 import { useNavigate, useParams } from 'react-router-dom'; // Importa useParams para obtener el parámetro de la URL
+import flecha from "../../assets/flecha.png";
 
 function TrabajosContainer() {
   const { userSearchResults } = useContext(JobsContext);
@@ -54,6 +55,7 @@ function TrabajosContainer() {
 
   return (
     <div id='ofertas' className='w-full flex flex-col items-center font-dmsans pt-6 px-4 justify-center pb-10'>
+       <h1 className='text-3xl font-bold mb-4'> <img src={flecha} alt="" className="mt-4 mb-5 inline-block w-16 h-12 mr-2"></img> OFERTAS <span className="text-amber-400 xl:inline"> LABORALES </span></h1>
       <div className='flex w-full justify-center items-center'>
         <div
           className='flex flex-col w-full md:w-1/2 justify-start items-center gap-4 h-[650px] overflow-auto md:pl-12 md:ml-10'
