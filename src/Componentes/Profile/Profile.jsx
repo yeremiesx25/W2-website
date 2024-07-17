7d0c893a9228fd4109ad7a0ef03565599f72c90b
Alexander0127 <AlexanderTR0127@gmail.com>
2024-07-17 11:31:55 -0500

    Asd

--- src/Componentes/Profile/Profile.jsx
+++ src/Componentes/Profile/Profile.jsx
@@ -181,16 +181,21 @@ function Profile() {
         </div>
 
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Subir CV
-          </label>
-          <input
+          <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue">
+            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+        </svg>
+        <span class="mt-2 text-base leading-normal">Select a file</span>
+        <input
             type="file"
-            className="mt-1 block"
+            className="hidden"
             accept=".pdf,.doc,.docx"
             onChange={handleCvUpload}
             disabled={!isEditing}
           />
+          </label>
+          
+          
           {cvFile && (
             <p className="mt-2 text-sm text-gray-500">{cvFile.name}</p>
           )}
