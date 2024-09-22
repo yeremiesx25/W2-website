89ecd9ffc15d56a9db728cecbcdeef8bf7c77f81
Alexander0127 <AlexanderTR0127@gmail.com>
2024-09-22 12:24:12 -0500

    integración de OpenAi

--- src/Componentes/Form/getAssistantResponse.jsx
+++ src/Componentes/Form/getAssistantResponse.jsx
@@ -1,14 +1,42 @@
 import axios from 'axios';
+import { supabase } from '../../supabase/supabase.config';  // Importa tu configuración de Supabase ya existente
 
-const apiKey = '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';
+// Función para obtener la clave desde Supabase
+const obtenerClave = async () => {
+  try {
+    const { data, error } = await supabase
+      .from('Claves')
+      .select('clave')
+      .single();
 
+    if (error) {
+      throw error;
+    }
+
+    return data.clave;  // Retorna el valor de la clave obtenida
+  } catch (error) {
+    console.error('Error al obtener la clave:', error);
+    return null;
+  }
+};
+
+// Función para obtener la respuesta del asistente utilizando la clave obtenida
 const getAssistantResponse = async (message) => {
   try {
+    const apiKey = await obtenerClave();  // Obtener la clave desde Supabase
+
+    if (!apiKey) {
+      throw new Error('No se pudo obtener la API key');
+    }
+
     const response = await axios.post(
       'https://api.openai.com/v1/chat/completions',
       {
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'system', content: 'Eres un asistente encargado de crear ofertas de trabajo atractivas y efectivas para puesto de trabajo operativo, eres capaz de generar el título del puesto, descripción, funciones, requisitos, beneficios, horario' }, { role: 'user', content: message }],
+        messages: [
+          { role: 'system', content: 'Eres un asistente encargado de crear ofertas de trabajo atractivas y efectivas para puesto de trabajo operativo, eres capaz de generar el título del puesto, descripción, funciones, requisitos, beneficios, horario' },
+          { role: 'user', content: message }
+        ],
         temperature: 0.7,
       },
       {
