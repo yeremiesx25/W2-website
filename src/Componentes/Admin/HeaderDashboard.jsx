bfd77472f26234018f54f589b36f939bc4930760
PieroNF <pgianmarco10@gmail.com>
2024-09-27 12:21:42 -0500

    asda

--- src/Componentes/Admin/HeaderDashboard.jsx
+++ src/Componentes/Admin/HeaderDashboard.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import BuscadorJob from './BuscadorJob'; // Asegúrate de que la ruta sea correcta
+import React, { useEffect, useState } from 'react';
+import BuscadorJob from './BuscadorJob';
+import { UserAuth } from '../../Context/AuthContext'; // Importa tu contexto de autenticación
 
 function HeaderDashboard() {
+  const { user } = UserAuth(); // Obtén el usuario desde el contexto
+  const [reclutadorNombre, setReclutadorNombre] = useState('');
+
+  useEffect(() => {
+    if (user && user.nombre) {
+      setReclutadorNombre(user.nombre); // Accede al nombre del usuario
+    } else {
+      setReclutadorNombre('Reclutador'); // Si no hay nombre, usa un valor predeterminado
+    }
+  }, [user]);
+
   return (
-    <div className="flex justify-between items-center pt-8  ">
+    <div className="flex justify-between items-center pt-8">
       <div>
-        <p className="text-sm text-gray-500">Hi Alexander,</p>
+        <p className="text-sm text-gray-500">Hola {reclutadorNombre}</p>
         <h1 className="text-3xl font-bold text-blue-900">Bienvenido a Power</h1>
       </div>
       <div className="w-1/4">
