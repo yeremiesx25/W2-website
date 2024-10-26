da6bdc11f7aeb7523e8e80126e9ec7090e561a8b
yeremiesx25 <j4r4my_2002@hotmail.com>
2024-10-25 19:10:32 -0500

    a

--- src/Componentes/PowerAuth/HeaderPowerAuth.jsx
+++ src/Componentes/PowerAuth/HeaderPowerAuth.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion'; // Importar framer motion
-import logo from '../../assets/LogoPower.png';
+import { motion } from 'framer-motion';
 import { UserAuth } from "../../Context/AuthContext";
 import { supabase } from "../../supabase/supabase.config";
 import { RxAvatar } from "react-icons/rx";
@@ -10,18 +9,15 @@ import { TiArrowSortedDown } from "react-icons/ti";
 
 function HeaderPowerAuth() {
   const [showMenu, setShowMenu] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
-  const { user, signOut } = UserAuth();
   const [menuOpen, setMenuOpen] = useState(false);
   const [hasShadow, setHasShadow] = useState(false);
-
-  // Estado para los datos del perfil
+  const { user, signOut } = UserAuth();
   const [profile, setProfile] = useState({
     nombre: '',
     avatar_url: ''
   });
 
-  // Función para obtener el perfil desde la tabla 'perfiles'
+  // Obtener perfil desde la tabla 'perfiles'
   const fetchProfile = async () => {
     try {
       const { data, error } = await supabase
@@ -52,24 +48,28 @@ function HeaderPowerAuth() {
     }
   }, [user]);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
   useEffect(() => {
     const handleScroll = () => {
       setHasShadow(window.scrollY > 0);
     };
-
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-  const handleToggleMenu = () => {
-    setMenuOpen((prevState) => !prevState);
-  };
+  // Detectar clic fuera del menú
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuOpen && !event.target.closest(".menu-content")) {
+        setMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuOpen]);
 
   return (
     <header className={`bg-newprimarycolor fixed w-full z-10 font-dmsans transition-shadow duration-300 ${hasShadow ? 'shadow-lg' : ''}`}>
@@ -77,25 +77,15 @@ function HeaderPowerAuth() {
         {/* Logo */}
         <div className="flex items-center">
           <a className='text-yellowprimary text-2xl font-medium' href="/PowerAuth">
-            {/* <img src={logo} alt="Power" className="w-24 h-auto" />  */}
             Power
           </a>
-        </div>
-
-        {/* Opciones de navegación */}
-        <div className="hidden md:flex justify-around items-center text-white text-md gap-4 font-dmsans">
-          <Link to="/PowerAuth" className="hover:text-yellowprimary">Inicio</Link>
-          <Link to="/Empresas" className="hover:text-yellowprimary">Empresas</Link>
-          <Link to="/DescubriendoTalentos" className="hover:text-yellowprimary">Descubriendo Talentos</Link>
-          <Link to="/Coworking" className="hover:text-yellowprimary">Coworking</Link>
-          <Link to="/PowerAuth" className="font-semibold text-yellowprimary hover:text-white underline decoration-yellowprimary underline-offset-4">Power</Link>
         </div>
 
         {/* Avatar y menú desplegable */}
         <div className="relative">
           <button 
             className="flex items-center justify-between focus:outline-none gap-2"
-            onClick={handleToggleMenu}
+            onClick={() => setMenuOpen(!menuOpen)}
           >
             <span className="overflow-hidden whitespace-nowrap overflow-ellipsis text-white font-regular w-auto hidden md:flex items-center">
               {profile.nombre || 'Usuario'}
@@ -108,46 +98,32 @@ function HeaderPowerAuth() {
             />
           </button>
 
-          {/* Menú desplegable con animación de Framer Motion */}
+          {/* Menú desplegable */}
           {menuOpen && (
-            <motion.div 
-              initial={{ opacity: 0, y: -10 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10 }}
-              className="absolute right-0 w-48 bg-white rounded-md shadow-lg py-2 z-10"
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              className="absolute right-0 w-56 bg-gradient-to-r from-white to-gray-100 rounded-lg shadow-2xl py-4 z-20 border border-gray-200 menu-content"
             >
               <Link
                 to="/Profile1"
-                className="flex px-4 py-2 text-newprimarycolor hover:bg-blue-50"
+                className="flex items-center px-6 py-3 text-gray-800 hover:bg-gradient-to-r hover:from-blue-100 hover:to-blue-50 transition-all duration-300 rounded-md"
               >
-                <RxAvatar size={24} className='mr-2' />
-                Perfil
+                <RxAvatar size={26} className="mr-3 text-blue-600" />
+                <span className="font-semibold text-sm">Mi Perfil</span>
               </Link>
               <button
                 onClick={signOut}
-                className="flex items-center px-4 py-2 w-full text-red-600 hover:bg-blue-50"
+                className="flex items-center px-6 py-3 w-full text-red-500 hover:bg-gradient-to-r hover:from-red-100 hover:to-red-50 transition-all duration-300 rounded-md mt-2"
               >
-                <BiLogOut size={24} />
-                <span className="ml-2">Cerrar sesión</span>
+                <BiLogOut size={26} className="mr-3" />
+                <span className="font-semibold text-sm">Cerrar sesión</span>
               </button>
             </motion.div>
           )}
         </div>
       </div>
-
-      {/* Menú desplegable para dispositivos móviles */}
-      {showMenu && (
-        <div className="md:hidden bg-gray-900 w-full">
-          <div className="px-4 pt-2 pb-3 space-y-1">
-            <Link to="/PowerAuth" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Inicio</Link>
-            <Link to="/Empresas" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Empresas</Link>
-            <Link to="/DescubriendoTalentos" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Descubriendo Talentos</Link>
-            <Link to="/Coworking" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Coworking</Link>
-            <Link to="/Power" className="text-white hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium">Power</Link>
-          </div>
-        </div>
-      )}
-      {modalOpen && <Login closeModal={handleModalClose} />}
     </header>
   );
 }
